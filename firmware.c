26f94ff42cf131b7895eb0d9731908b307c54a84
namedummy568-stack <namedummy568@gmail.com>
2025-09-21 16:52:56 +0530

    Fixed communication bug with sensor module

--- firmware.c
+++ firmware.c
@@ -3,6 +3,7 @@ void setup() {
   // Initialize basic control loop
   // Added sensor calibration routine
   // Implemented power saving mode
+  // Fixed communication bug with sensor module
 }
 void loop() {
   // Run basic control loop
